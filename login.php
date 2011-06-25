ce0aa9df674949cc655885926393c8b26088cbbc
Matthias <obiwan@marsmail.de>
2011-06-25 17:39:02 +0200

    login.php auskommentiert und code verschönert

--- login.php
+++ login.php
@@ -1,7 +1,14 @@
 <?php
-	include 'config.php';
 
-	function anzeigenFormular(){
+// Einbinden der Zugriffsdaten für die Datenbank und erstellen eines Datenbankobjekts
+
+	include 'config.php';
+	$db = new mysqli(HOST, USER, PASS, DB);
+	
+/* Funktion anzeigenFormular zeigt die Patienten und ob für Patient ein Formular gepeichert ist
+   Wird aufgerufen wenn der Benutzer eingeloggt ist */
+
+      function anzeigenFormular(){
 	$db = new mysqli(HOST, USER, PASS, DB);
 	echo '<form action="formular.php?PHPSESSID="'.session_id().' method="post">';
 	echo '<fieldset>';
@@ -31,7 +38,14 @@
 	echo '</form>';
 	}
 
+
+
+// Eine Session wird gestartet
+
 	session_start();
+
+// Das Design, inklusive Menü wird geladen
+
   	include 'head2.html';
     		echo '<li><a href=\'login.php?PHPSESSID='.session_id().'\'>Home</a></li>';
     		echo '<li><a href=\'logout.php?PHPSESSID='.session_id().'\'>Logout</a></li>';
@@ -39,11 +53,12 @@
     	  echo '</ul>';
   	  echo '</div>';
 	echo '</div>';
-	
-
 	echo '<div id="content">';
 	echo '<div id="label">';
 	echo '<div id="box">';
+
+// Ab hier fängt der eigentliche Inhalt des Dokuments an
+
 	echo '<fieldset>';
 	echo '<legend>Status: </legend>';
 	if (!isset($_SESSION['permission'])||$_SESSION['permission']==false){
@@ -51,15 +66,24 @@
 	$_SESSION['password']=$_POST['password'];
 	}
 
-	$db = new mysqli(HOST, USER, PASS, DB);
+// Datensatz wird aus der Datenbank geladen bei dem der Benutzername und Passwort welche eingegeben wurden mit denen der Datenbank übereinstimmen
+
 	$sql = 'SELECT * FROM users INNER JOIN rollen ON users.rollenid = rollen.id WHERE username=\''.$_SESSION['user'].'\' AND password=\''.$_SESSION['password'].'\';';
 	$result = $db->query($sql);
 	$userfound = false;
+
+/* Wenn sich ein Datensatz in $result befindet dann hat sich der User mit dem richtigem Benutzernamen und Passwort eingewählt,
+  was mit der folgenden if-Abfrage geprüft wird. */
+
 	if ($row = $result->fetch_assoc()){
 		$userfound = true;
 	}
+
+
+// Wenn der Benutzer gefunden wurde wird die Seite geladen, andernfalls wird ausgegeben: Falscher Benutzername und/oder Passwort !
+
 	if ($userfound){
-	echo '<div id="foto"><img src="fotos/foto'.$row['userid'].'.jpg" alt="foto" width="110" height="140"></div>';
+ 	        echo '<div id="foto"><img src="fotos/foto'.$row['userid'].'.jpg" alt="foto" width="110" height="140"></div>';
 		$_SESSION['permission'] = true;
 		$_SESSION['reading'] = $row['read'];
 		$_SESSION['writing'] = $row['write'];
@@ -68,20 +92,25 @@
 		echo 'Nachname: '.$row['lastname'].'<br>';
 		echo 'Leserecht: ';
 		if ($row['read']) echo 'Ja'.'<br>'; else echo 'Nein'.'<br>';
-		
 		echo 'Bearbeitungsrecht: ';
 		if ($row['write']) echo 'Ja'.'<br>'; else echo 'Nein'.'<br>';
   	        echo '</fieldset><br>';
+		
 		anzeigenFormular();
 	}
+
 	else
+
 	{
 		$_SESSION['permission'] = false;
-		echo 'Falscher Benutzername und/oder Passwort !<br>';
+		echo 'Falscher Benutzername und/oder Passwort !<br><br>';
  	        echo '<a href=\'index.html\'>Hier wieder einloggen</a>';
 		echo '</fieldset><br>';
 	}
 
     echo'</div>';
+
+// Das Dokumentenende wird reingeladen
+
   include 'bottom.html';
 ?>
